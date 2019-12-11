8ac55e57d8740adad1bee15dfeaabfbb29ea89cc
nazifkassim96 <57617138+nazifkassim96@users.noreply.github.com>
2019-12-11 17:11:40 +0800

    Update and rename Model/Announcement.php to Include/AdminHeader.php

---
+++ Include/AdminHeader.php
@@ -0,0 +1,13 @@
+<div>
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="AdministratorHomePage.php" class="w3-bar-item w3-button w3-padding-large w3-white">HOME</a>
+    <a href="AdministratorProfilePage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">PROFILE</a>
+    <a href="ManageCarPage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">VEHICLE</a>
+    <a href="PaymentRecord.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">PAYMENT</a>
+    <a href="RentAndReturnPage.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">RENT & RETURN</a>
+    <a href="AdministratorList.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">ADMIN LIST</a>
+	<a href="Logout.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">LOGOUT</a>
+  </div>
+</div>
+<br><br>
